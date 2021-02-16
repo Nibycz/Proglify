1108ac47e8dca7e2d1760a0cae13c9fe2280b15c
emrah <important.esati@web.de>
2021-02-16 19:45:21 +0100

    Add HttpStatus OK and Remove useless controllermethod

--- src/main/java/com/progly/progly/controller/UserController.java
+++ src/main/java/com/progly/progly/controller/UserController.java
@@ -3,6 +3,7 @@ package com.progly.progly.controller;
 import com.progly.progly.model.User;
 import com.progly.progly.service.UserService;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -20,15 +21,9 @@ public class UserController {
         this.userService = userService;
     }
 
-
-    @GetMapping(path="test")
-    public void test(){
-        System.out.println("123");
-    }
-
     @PostMapping(path = "registration")
-    @ResponseStatus(HttpStatus.CREATED)
-    public void register(@Valid @RequestBody User user){
+    public ResponseEntity<Long> register(@Valid @RequestBody User user){
         userService.addNewUser(user);
+        return new ResponseEntity<>(user.getId(), HttpStatus.OK);
     }
 }

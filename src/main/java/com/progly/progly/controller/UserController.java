9f0dbc80c5644299603e21eb7e189deb94d9b5cf
Emrah <emrah.esati@condat.de>
2021-02-25 14:15:18 +0100

    Refactor. Rebuilded something in the Registrationprocess.

--- src/main/java/com/progly/progly/controller/UserController.java
+++ src/main/java/com/progly/progly/controller/UserController.java
@@ -5,13 +5,17 @@ import com.progly.progly.model.dto.UserDto;
 import com.progly.progly.service.UserServiceImpl;
 import com.progly.progly.validator.UserValidator;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.WebDataBinder;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
+import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
 @Controller
 @RequestMapping(path = "api/v1/user")
@@ -19,21 +23,32 @@ public class UserController {
 
     private final UserServiceImpl userService;
 
-    @Autowired
-    private final UserValidator userValidator;
-
-    public UserController(UserServiceImpl userService, UserValidator userValidator) {
+    public UserController(UserServiceImpl userService) {
         this.userService = userService;
-        this.userValidator = userValidator;
     }
 
     @PostMapping(path = "registration")
-    public void createNewUser(@Valid @RequestBody UserDto userDto, BindingResult results) {
-        userValidator.validate(userDto, results);
+    public ResponseEntity createNewUser(@Valid @RequestBody UserDto userDto, BindingResult results) {
+        // Valid prüft über die Annotation nach Fehlern (im DTO festgelegt). BindingResult holt sich die Daten (Fehler).
+        // Falls Fehler vorhanden sind werden diese in eine Liste von Strings gespeichert(errors). Danach wird ein Response zurückgegeben.
+        Boolean passwordMatch = userDto.checkPasswordMatching();
+
         if (results.hasErrors()) {
-            System.out.println(results.getFieldErrors());
+            List<String> errors = results.getAllErrors().stream().map(e -> e.getDefaultMessage()).collect(Collectors.toList());
+            if(!passwordMatch) errors.add("Passwörter stimmen nicht überein.");
+            return new ResponseEntity(errors, HttpStatus.BAD_REQUEST);
         }
+
+        if(!passwordMatch) return new ResponseEntity("Passwörter stimmen nicht überein.", HttpStatus.BAD_REQUEST);
+
         Optional<User> userExists = userService.findUserByEmail(userDto.getEmail());
+        if(userExists.isPresent()){
+            return new ResponseEntity(String.format("Ein Benutzer mit der Email-Adresse %s ist bereits vorhanden.", userDto.getEmail()), HttpStatus.BAD_REQUEST);
+        } else {
+            userService.createUserAccount(userDto);
+        }
+
+        return new ResponseEntity(HttpStatus.OK);
     }
 
 }
